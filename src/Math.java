e147a861894bc50462f54eb354fd8047788d0392
Omobolaji Ganiu Adebola <omobolaji.adebola@gmail.com>
2025-10-14 23:23:55 +0100

    Added Multi function by Omobolaji

---
+++ src/Math.java
@@ -0,0 +1,13 @@
+package src;
+
+//Developer: Omobolaji Ganiu Adebola
+//University ID: 240106205
+//Function: This function takes two integers as input, multiplies both numbers, and returns the result.
+
+public class Math {
+	public int Multi(int x, int y) {
+        return x * y;
+    }
+
+ 
+}
