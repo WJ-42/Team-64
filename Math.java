d31987bd8f8ceffc67507e11dc62f93a7066d1d6
Jessica McEnery <240177700@aston.ac.uk>
2025-10-14 17:10:36 +0100

    Added Divide function - Jessica McEnery

--- Math.java
+++ Math.java
@@ -13,3 +13,15 @@ public int Power(int base, int exponent) {
     }
     return result;
 }
+
+/********************************
+Developer: Jessica McEnery
+University ID: 240177700
+Function: Divide - takes two integers as input, divides the first number by the second, returns result as a double. Throws exception to avoid infinity.
+********************************/
+public static double Divide(int a, int b) {
+    if (b == 0) {
+        throw new IllegalArgumentException("Cannot divide by zero");
+    }
+    return (double) a / b;
+}
