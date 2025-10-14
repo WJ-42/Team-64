9d0a5d354f433f8a91fce71d001301a00f39674d
Waleed Ahmed <240048282@aston.ac.uk>
2025-10-14 13:37:45 +0100

    Created Math class file for team activity

---
+++ Math.java
@@ -0,0 +1,7 @@
+/********************************
+Developer: Waleed Jawad
+University ID: (240048282)
+Purpose: This class contains basic math functions for the team activity.
+********************************/
+public class Math {
+}
