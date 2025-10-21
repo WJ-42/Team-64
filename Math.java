7756b5d31fc46a5c6f4cd100aa4cf0aa61f9bdec
Jessica McEnery <240177700@aston.ac.uk>
2025-10-21 13:28:27 +0100

    Created Divide

--- Math.java
+++ Math.java
@@ -1,24 +1,8 @@
 /********************************
-Developer: Waleed Ahmed
-University ID: 240048820
-Function: Power – returns base raised to exponent (both ints)
-********************************/
-public int Power(int base, int exponent) {
-    if (exponent < 0) {
-        return 0; // simple handling for negative exponents
-    }
-    int result = 1;
-    for (int i = 0; i < exponent; i++) {
-        result *= base;
-    }
-    return result;
-}
-
-/********************************
 Developer: Jessica McEnery
 University ID: 240177700
 Function: Divide - takes two integers as input, divides the first number by the second, returns result as a double. Throws exception to avoid infinity.
-********************************/
+********************************/ double
 public static double Divide(int a, int b) {
     if (b == 0) {
         throw new IllegalArgumentException("Cannot divide by zero");
