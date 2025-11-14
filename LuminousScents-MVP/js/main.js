c7dcfd02c9a9522a2fb315222122be93c918fa2e
Waleed Ahmed <240048282@aston.ac.uk>
2025-11-14 11:36:53 +0000

    Make footer fade in as soon as any it enters view.

--- LuminousScents-MVP/js/main.js
+++ LuminousScents-MVP/js/main.js
@@ -427,8 +427,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 // Scroll reveal animations
 const observerOptions = {
-    threshold: 0.1,
-    rootMargin: '0px 0px -50px 0px'
+    threshold: 0,
+    rootMargin: '0px'
 };
 
 const observer = new IntersectionObserver((entries) => {
